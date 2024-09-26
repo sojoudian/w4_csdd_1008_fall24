85ecc00c4e9720b305a61655fe144eaa62f247fa
Maziar Sojoudian <maziar@sojoudian.net>
2024-09-26 19:06:25 -0400

    add two var return

---
+++ calc.go
@@ -0,0 +1,7 @@
+package main
+
+func calc_n(a, b int) (int, int) {
+	sum := a + b
+	product := a * b
+	return sum, product
+}
--- main.go
+++ main.go
@@ -5,4 +5,5 @@ import "fmt"
 func main() {
 	greeting()
 	fmt.Println(AddInt(1, 3))
+	fmt.Println(calc_n(2, 3))
 }
